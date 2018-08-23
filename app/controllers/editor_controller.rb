984d327f5408458be2cf7c66a4b7174997d2efc5
McElhone <Maxwell.McElhone@southeasttech.edu>
2018-08-23 11:45:46 -0500

    I Added a show method and view for viewing the document after creation. All the routing is working so far.

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -4,9 +4,26 @@ class EditorController < ApplicationController
   end
 
   def new
-    
+
   end
+
+  def show
+    @document = Document.find(params[:id])
+  end
+
   def create
-    render plain: params[:document].inspect
+    #this just outputs the properties of the object passed by the view as text.
+    #render plain: params[:document].inspect
+    @document = Document.new(document_params)
+
+    #save the object
+    @document.save
+
+    #redirect to that page
+    redirect_to action: "show", id: @document.id
+  end
+  #method to say what paramaters are needed to create the object
+  private def document_params
+    params.require(:document).permit(:title, :body)
   end
 end
