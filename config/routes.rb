091f362ca1bedb77e8e8d7cad9d8b09e2c56237a
McElhone <Maxwell.McElhone@southeasttech.edu>
2018-08-23 14:17:50 -0500

    Edited the navbar, added a helper method
    I added routing for the navagation bar and also created a helper method to dynamically add the active class.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #default route that the application takes and alias of home for the path
   root 'editor#index', as: 'home'
+  #creates most of the paths for the editor class
   resources :editor
+  #the one that it didn't make but I needed
   post "editor/new"    => "editor#create"
 end
