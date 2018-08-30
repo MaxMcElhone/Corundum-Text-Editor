39c13cddb0f346ca21045e3bf780ac14dfd9007c
McElhone <Maxwell.McElhone@southeasttech.edu>
2018-08-30 13:33:58 -0500

    Little changes
    the nav bar stays at the top, you can tab in textareas, and steven's function has been applied.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,54 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//
+function Upload() {
+  var file = $("#file").prop("files")[0];
+  fr = new FileReader(); // FileReader instance
+				      // Do stuff on onload, use fr.result for contents of file
+              //fr.readAsDataURL( file );
+              fr.readAsText( file );
+				      $( '#document_body' ).val(  fr.result  )
+
+
+};
+//method steven wrote to handle uploaded files slightly edited
+function openFile(event) {
+  var input = event.target;
+
+  var r = new FileReader();
+  r.onload = function(){
+    var text = r.result;
+    var out = document.getElementById('document_body');
+    out.value = text;
+    console.log(reader.result.substring(0,200));
+
+  };
+  r.readAsText(input.files[0]);
+
+};
+
+$( document ).on('turbolinks:load', function() {
+//allow tabbing in textarea
+  $("textarea").keydown(function(e) {
+      if(e.keyCode === 9) { // tab was pressed
+          // get caret position/selection
+          var start = this.selectionStart;
+          var end = this.selectionEnd;
+
+          var $this = $(this);
+          var value = $this.val();
+
+          // set textarea value to: text before caret + tab + text after caret
+          $this.val(value.substring(0, start)
+                      + "\t"
+                      + value.substring(end));
+
+          // put caret at right position again (add one for the tab)
+          this.selectionStart = this.selectionEnd = start + 1;
+
+          // prevent the focus lose
+          e.preventDefault();
+      }
+  });
+});
