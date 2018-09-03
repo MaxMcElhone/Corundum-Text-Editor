0240f57124b844ecdabe140cb7b9c20c137f4502
MaxMcElhone <42582959+MaxMcElhone@users.noreply.github.com>
2018-09-03 18:33:59 -0500

    tabbing in textareas is fixed again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -54,25 +54,25 @@ $( document ).on('turbolinks:load', function() {
       });
     });
 //allow tabbing in textarea
-  $("textarea").keydown(function(e) {
-      if(e.keyCode === 9) { // tab was pressed
-          // get caret position/selection
-          var start = this.selectionStart;
-          var end = this.selectionEnd;
-
-          var $this = $(this);
-          var value = $this.val();
-
-          // set textarea value to: text before caret + tab + text after caret
-          $this.val(value.substring(0, start)
-                      + "\t"
-                      + value.substring(end));
-
-          // put caret at right position again (add one for the tab)
-          this.selectionStart = this.selectionEnd = start + 1;
-
-          // prevent the focus lose
-          e.preventDefault();
-      }
-  });
+  // $("textarea").keydown(function(e) {
+  //     if(e.keyCode === 9) { // tab was pressed
+  //         // get caret position/selection
+  //         var start = this.selectionStart;
+  //         var end = this.selectionEnd;
+  //
+  //         var $this = $(this);
+  //         var value = $this.val();
+  //
+  //         // set textarea value to: text before caret + tab + text after caret
+  //         $this.val(value.substring(0, start)
+  //                     + "\t"
+  //                     + value.substring(end));
+  //
+  //         // put caret at right position again (add one for the tab)
+  //         this.selectionStart = this.selectionEnd = start + 1;
+  //
+  //         // prevent the focus lose
+  //         e.preventDefault();
+  //     }
+  // });
 });
